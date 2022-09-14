df51e4eef2ded480524b26bc8d1bc6d26fb42601
Miguel Retroz <miguel.retroz@6place.com.br>
2022-09-14 13:56:01 -0300

    feat: add input to the user enter a guess number

--- guess.c
+++ guess.c
@@ -4,4 +4,14 @@ int main() {
   printf("****************************************\n");
   printf("*Bem vindo ao nosso jogo de adivinhacão*\n");
   printf("****************************************\n");
+
+  int secretNumber = 42;
+
+  int guess;
+
+  printf("Qual o seu chute? ");
+  scanf("%d", &guess);
+
+  printf("\n...\n\n");
+  printf("Seu chute foi %d\n", guess);
 }
